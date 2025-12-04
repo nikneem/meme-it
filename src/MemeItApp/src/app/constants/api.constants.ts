e02136e6fd303b02b2abc2cfbfc413263e09935c
nikneem <ekeilholz+git@outlook.com>
2025-12-04 22:44:51 +0100

    Updates API base URL to production
    Updates the API base URL to point to the production environment.
    This ensures the application uses the live API endpoint instead of the local development server.

--- src/MemeItApp/src/app/constants/api.constants.ts
+++ src/MemeItApp/src/app/constants/api.constants.ts
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = 'https://api-gateway.proudmushroom-2fb47031.northeurope.azurecontainerapps.io'
-export const API_BASE_URL = 'http://localhost:5000'
+export const API_BASE_URL = 'https://api-gateway.proudmushroom-2fb47031.northeurope.azurecontainerapps.io'
+//export const API_BASE_URL = 'http://localhost:5000'
